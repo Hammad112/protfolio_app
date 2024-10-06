3cc026dd699226d79706239d0a5f7c509f66b67a
Hammad Nasir <95902997+Hammad112@users.noreply.github.com>
2024-10-07 00:56:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelector('#yourForm').addEventListener('submit', async (e) => {
     const email = document.querySelector('#email').value;
     const message = document.querySelector('#message').value;
 
-    const response = await fetch('https://your-backend-url/submit', {
+    const response = await fetch('https://3eaf61ef-0c14-4730-8f62-862f2de875ba.e1-us-east-azure.choreoapps.dev/submit', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
